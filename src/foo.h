e38b7220b6da707bc7a6fe5b25549e48743711df
Daria <ogloblik@yandex.ru>
2019-04-17 12:16:43 +0700

    Fixed bugs of the format

--- src/foo.h
+++ src/foo.h
@@ -1,7 +1,23 @@
 #ifndef H_FOO
 #define H_FOO
-float plo(char figure[10], float x1, float y1, float x2, float y2, float x3,
-	  float y3, float x4, float y4);
-float per(char figure[10], float x1, float y1, float x2, float y2, float x3,
-	  float y3, float x4, float y4);
+float plo(
+        char figure[10],
+        float x1,
+        float y1,
+        float x2,
+        float y2,
+        float x3,
+        float y3,
+        float x4,
+        float y4);
+float per(
+        char figure[10],
+        float x1,
+        float y1,
+        float x2,
+        float y2,
+        float x3,
+        float y3,
+        float x4,
+        float y4);
 #endif
